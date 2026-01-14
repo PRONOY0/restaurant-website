8ca980daa8f97605890d5601bedac792d311d4cb
Pronoy <pronoyroy782@gmail.com>
2026-01-14 22:32:51 +0530

    done

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -8,10 +8,6 @@ const AboutUs = () => (
     className="app__aboutus app__bg flex__center section__padding"
     id="about"
   >
-    <div className="app__aboutus-overlay flex__center">
-      <img src={images.G} alt="G_overlay" />
-    </div>
-
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
